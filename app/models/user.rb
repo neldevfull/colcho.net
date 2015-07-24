07f789a52880489adb7343cd786998d8688e60fe
neldevfull <nfrugeri@gmail.com>
2015-07-24 09:12:31 -0300

    *** COMMIT - Finished User class validations ***

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,20 @@
 class User < ActiveRecord::Base
+
+	EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/ 
+
 	validates_presence_of :email, :full_name, :location, :password
 	validates_confirmation_of :password 
 	validates_length_of :bio, minimun: 30, allow_blank: false
-end
+	validates_uniqueness_of :email 
+
+	validate :email_format   
+
+	private
+
+	# This validation can be represented as follows
+	# validates_format_of :email, with: EMAIL_REGEXP
+	def email_format
+		erros.add(:email, :invalid) unless email.match(EMAIL_REGEXP)
+	end
+
+end  
